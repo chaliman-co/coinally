9de9e8fe5ea1b6bb41925cebe0e63f99b0276b2a
chaliman-co <chaliman.co@gmail.com>
2018-06-08 16:49:36 +0100

    switched to  history mode

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const serverUtils = require('../lib/utils');
+const path = require('path');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    return res.sendFile('../public/index.html');
+router.get('*', function(req, res, next) {
+    return res.sendFile(path.join(serverUtils.getPublicDirectory(), 'index.html'))
 });
 
 module.exports = router;
