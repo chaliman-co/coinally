e6b865da3b9903ca2a4c1991e3b79a012ddf5764
Chidozie Ogbo <chidozieogbo@outlook.com>
2018-07-07 09:31:09 +0100

    #Inner Transaction * Added transaction modal * Did some refactoring

---
+++ src/enums.js
@@ -0,0 +1,7 @@
+export const txStatus = {
+    FAILED: 'failed',
+    PENDING: 'pending',
+    AWAITING_PAYMENT: 'awaiting payment',
+    PAYMENT_RECEIVED: 'payment received',
+    COMPLETED: 'completed',
+};
